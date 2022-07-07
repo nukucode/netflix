b8dbbdfe83fb2f09b1376ae3a964743a3e1b5ffe
usersploit <ankityadavmanish123@gmail.com>
2022-07-07 08:01:55 +0530

    Fix Error

--- src/App.js
+++ src/App.js
@@ -5,53 +5,21 @@ import Footer from "./Footer";
 import Nav from "./Nav";
 import requests from "./Request";
 import Row from "./Row";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
-        <Router>
-          <Routes>
-
-            <Route
-              path="/"
-              element={
-                <>
-                <Nav />
-                  <Banner />
-                  <Row
-                    title="Netflix Originals"
-                    fetch={requests.fetchNetflixOriginals}
-                  />
-                  <Row title="Trending" fetch={requests.fetchTrending} isTop />
-                  <Row title="Top Rated" fetch={requests.fetchTopRated} />
-                  <Row
-                    title="Action Movies"
-                    fetch={requests.fetchActionMovies}
-                  />
-                  <Row
-                    title="Comedy Movies"
-                    fetch={requests.fetchComedyMovies}
-                  />
-                  <Row
-                    title="Horror Movies"
-                    fetch={requests.fetchHorrorMovies}
-                  />
-                  <Row
-                    title="Romance Movies"
-                    fetch={requests.fetchRomanceMovies}
-                  />
-                  <Row
-                    title="Documentários"
-                    fetch={requests.fetchDocumentaries}
-                  />
-                  <Footer />
-                </>
-              }
-            />
-            
-          </Routes>
-        </Router>
+      <Nav />
+      <Banner />
+      <Row title="Netflix Originals" fetch={requests.fetchNetflixOriginals} />
+      <Row title="Trending" fetch={requests.fetchTrending} isTop />
+      <Row title="Top Rated" fetch={requests.fetchTopRated} />
+      <Row title="Action Movies" fetch={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetch={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetch={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetch={requests.fetchRomanceMovies} />
+      <Row title="Documentários" fetch={requests.fetchDocumentaries} />
+      <Footer />
     </>
   );
 }
