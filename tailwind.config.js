4f317e2952916c875a0b6b3f2d92969e762018ca
Ankit Yadav <therogersak@gmail.com>
2024-09-10 20:24:42 +0530

    💄 add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,css,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans':["open-sans"]
+      }
+    },
+  },
+  plugins: [],
+}
+
