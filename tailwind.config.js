15c5e9ebba106981f417fdaa0e52a0c84f155317
Ankit Yadav <therogersak@gmail.com>
2024-09-13 13:56:58 +0530

    ✅ colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,css,js}"],
   theme: {
+    // Custom Colors
+    colors: {
+      transparent: "transparent",
+      currentColor: "currentColor",
+      netflixColor: "#E50914",
+      white: "#FFFFFF",
+      black: "#000000",
+      gray: "#595959",
+    },
+    // Custom Fonts
     extend: {
       fontFamily: {
         sans: ["open-sans", "sans-serif"],
